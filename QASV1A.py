bb9dfc5cbfee83716a9a07b90002b329b4ff02b9
Nadia7000 <blueeye7007@gmail.com>
2020-07-29 20:36:47 -0700

    New update!

---
+++ QASV1A.py
@@ -0,0 +1,12 @@
+thislist = ["4", "2", "7", "1", "9"]
+for x in thislist:
+  print(x)
+
+thislist1 = []
+thislist1.append("5")
+thislist1.append("-6")
+print(thislist1)
+
+thislist2 = thislist + thislist1
+print(thislist2)
+print(thislist1)
